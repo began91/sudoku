e0b10b8e6d44947c22a47affb0eb50a85797a129
began91 <began91@yahoo.com>
2020-06-28 14:52:12 -0500

    Updates

---
+++ precache-manifest.859934710e92a52c56d75d1b6bc6d1da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6942313a2094ca07a1d4b07ea5d91fa5",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "a09f4cc4a9925d20d861",
+    "url": "/sudoku/static/css/main.1ab8a6bd.chunk.css"
+  },
+  {
+    "revision": "10a44b66754d11931f5b",
+    "url": "/sudoku/static/js/2.94770168.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sudoku/static/js/2.94770168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09f4cc4a9925d20d861",
+    "url": "/sudoku/static/js/main.39d904e1.chunk.js"
+  },
+  {
+    "revision": "8d8e892f47ac5d31712b",
+    "url": "/sudoku/static/js/runtime-main.f7976529.js"
+  }
+]);
